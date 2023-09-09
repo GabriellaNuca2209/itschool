c5ec924ebf5479ce3038af94907c37d2af6b0eb1
Gabriella Nuca <gabriellanuca@gmail.com>
2023-09-09 22:50:02 +0300

    First 3 challenges done

--- src/session3/challenges/LogicalComplementChallenge.java
+++ src/session3/challenges/LogicalComplementChallenge.java
@@ -1,8 +1,30 @@
 package session3.challenges;
 
+import java.util.Scanner;
+
 public class LogicalComplementChallenge {
 
     public static void main(String[] args) {
+        String result = isPositiveOrNegative(-4);
+        System.out.println("The number is " + result);
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type first boolean");
+        boolean bool1 = scanner.nextBoolean();
+        System.out.println("Type second boolean");
+        boolean bool2 = scanner.nextBoolean();
+
+        System.out.println(bool1 + " -> " + !bool1);
+        System.out.println(bool2 + " -> " + !bool2);
+
+        scanner.close();
+    }
+
+    public static String isPositiveOrNegative(int number) {
+        if (number > 0) {
+            return "positive";
+        } else {
+            return "negative";
+        }
     }
 }
--- src/session3/challenges/UnaryChallenge.java
+++ src/session3/challenges/UnaryChallenge.java
@@ -3,6 +3,17 @@ package session3.challenges;
 public class UnaryChallenge {
 
     public static void main(String[] args) {
+        int number = 25;
+        int incremented = ++number;
+        System.out.println(incremented);
+        int decremented = --number;
+        System.out.println(decremented);
 
+        int reversed =  reverseSign(25);
+        System.out.println(reversed);
+    }
+
+    public static int reverseSign(int n) {
+        return -n;
     }
 }
