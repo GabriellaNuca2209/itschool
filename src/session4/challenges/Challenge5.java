c7a0c6882e154096c1617136e1ddd40068c966f3
Gabriella Nuca <gabriellanuca@gmail.com>
2023-09-14 23:51:14 +0300

    Session 4 first 7 challenges

---
+++ src/session4/challenges/Challenge5.java
@@ -0,0 +1,18 @@
+package session4.challenges;
+
+public class Challenge5 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("-------------");
+
+        for (int j = 0; j <=10; j++) {
+            if (j % 2 == 0) {
+                System.out.println(j);
+            }
+        }
+    }
+}
