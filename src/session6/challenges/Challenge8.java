1a60f50939dd0a47e0a1cfe7bc9e80e1f2d71f55
Gabriella Nuca <gabriellanuca@gmail.com>
2023-09-20 16:24:25 +0300

    Session 6, challenges 6-8

--- src/session6/challenges/Challenge8.java
+++ src/session6/challenges/Challenge8.java
@@ -3,6 +3,14 @@ package session6.challenges;
 public class Challenge8 {
 
     public static void main(String[] args) {
+        String string = "dear";
+        insertString(string, 3);
+    }
 
+    public static void insertString(String str, int index) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Hi, John");
+        stringBuilder.insert(index, str);
+        System.out.println(stringBuilder);
     }
 }
--- src/session6/challenges/Challenge9.java
+++ src/session6/challenges/Challenge9.java
@@ -3,6 +3,12 @@ package session6.challenges;
 public class Challenge9 {
 
     public static void main(String[] args) {
+        String string = "telecommunication";
+        countLetterFrequency(string);
+    }
+
+    public static void countLetterFrequency(String str) {
+        StringBuilder stringBuilder = new StringBuilder();
 
     }
 }
