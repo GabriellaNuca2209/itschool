f85cf5c6f5e28f3a8a1709759374dbc1b441b66d
Gabriella Nuca <gabriellanuca@gmail.com>
2023-09-24 23:43:29 +0300

    Session 7, challenges 1-10

--- src/session7/practice/Student.java
+++ src/session7/practice/Student.java
@@ -5,7 +5,7 @@ public class Student {
     String name;
     int age;
 
-    public void displayDetails(){
+    public void displayDetails() {
         System.out.println("Name: " + name + ", Age: " + age);
     }
 }
