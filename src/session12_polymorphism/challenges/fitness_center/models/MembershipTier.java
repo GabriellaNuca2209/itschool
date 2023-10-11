799654ec541dcf7f6abf2f7af04eea4fb94e81c9
Gabriella Nuca <gabriellanuca@gmail.com>
2023-10-11 17:24:32 +0300

    session 12, fitness draft

---
+++ src/session12_polymorphism/challenges/fitness_center/models/MembershipTier.java
@@ -0,0 +1,7 @@
+package session12_polymorphism.challenges.fitness_center.models;
+
+public enum MembershipTier {
+    BASIC,
+    PREMIUM,
+    PLATINUM
+}
---
+++ src/session12_polymorphism/challenges/fitness_center/services/BookingService.java
@@ -0,0 +1,15 @@
+package session12_polymorphism.challenges.fitness_center.services;
+
+import session12_polymorphism.challenges.fitness_center.models.FitnessClass;
+import session12_polymorphism.challenges.fitness_center.models.Member;
+
+public class BookingService {
+
+    public static void checkAvailability(FitnessClass fitnessClass) {
+
+    }
+
+    public static void createBooking(FitnessClass fitnessClass, Member member) {
+
+    }
+}
