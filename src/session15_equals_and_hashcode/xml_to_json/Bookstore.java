76474e4c3412633fcd8c69e079c5dd2500d2b9d9
Gabriella Nuca <gabriellanuca@gmail.com>
2023-10-24 02:19:21 +0300

    session 15, tried xml to json

---
+++ src/session15_equals_and_hashcode/xml_to_json/Bookstore.java
@@ -0,0 +1,40 @@
+package session15_equals_and_hashcode.xml_to_json;
+
+public class Bookstore {
+    private String isbn;
+    private String title;
+    private String author;
+    private Double price;
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
